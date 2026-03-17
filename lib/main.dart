3c83c71e3979a8c5c099e1a13a5756311f64fb87
sktenorio <imbamik@gmail.com>
2026-03-17 10:53:41 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // 3. Initialize Supabase
-  await Supabase.initialize(
-    url: 'https://ttsrktldvvqrgkfhsbbl.supabase.co/',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0c3JrdGxkdnZxcmdrZmhzYmJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NDYxODcsImV4cCI6MjA4ODEyMjE4N30.DkQAOtyA4gezkPFCWPtyoS2UKw2NYvZcAlsAWbql3QY',
+  await   .initialize(
+    url: 'http://136.111.137.86:8000',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzczNjMyMDI2LCJleHAiOjE5MzEzMTIwMjZ9.3F7YOLt761b6G1OkIlDTG_70BNUMTe8nt5fnsBN98dY',
   );
 
   runApp(const FlooteApp());
